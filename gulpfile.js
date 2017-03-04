d78e5d9a8a970519493ccff17eca0d6582218297
VaskM <vaskm@outlook.com>
2017-03-04 11:28:33 +0200

    personal blog init

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp    = require('gulp'),
+browserSync = require('browser-sync'),
+cache       = require('gulp-cached'),
+clean       = require('gulp-clean'),
+progeny     = require('gulp-progeny'),
+imageMin    = require('gulp-imagemin'),
+useref      = require('gulp-useref'),
+gulpif      = require('gulp-if'),
+uglify      = require('gulp-uglify'),
+compass     = require('gulp-compass'),
+minifyCss   = require('gulp-minify-css');
+
+/*CLEAR*/
+gulp.task('clean', function(){
+	return gulp.src('dist/', {read: false})
+	.pipe(clean())
+})
+
+/*Сборка в определенном порядке*/
+gulp.task('build', ['clean', 'compass', 'css', 'images', 'fonts'], function(){
+	return gulp.src('app/**/*.html')
+	.pipe(gulpif('*.js', uglify()))
+	.pipe(gulpif('*.css', minifyCss()))
+	.pipe(useref())
+	.pipe(gulp.dest('dist'));
+})
+/*COMPASS*/
+gulp.task('compass', function () {
+  return gulp.src('app/sass/**/*.+(sass|scss)') // получаем источники с помощью gulp.src
+  .pipe(cache('compass'))
+  .pipe(progeny())
+  .pipe(compass({
+	config_file: './config.rb',
+	css: './app/css',
+	sass: './app/sass',
+  }))
+	.pipe(gulp.dest('app/css/')) // выходные файлы в папке destination
+	.pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('css', function(){
+
+	return gulp.src('app/css/**/*.css')
+	.pipe(gulp.dest('dist/css/'))
+})
+
+/*IMAGES*/
+gulp.task('images', function(){
+	gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+	.pipe(imageMin({interlaced:true}))
+	.pipe(gulp.dest('dist/images/'))
+});
+/*FONTS*/
+gulp.task('fonts', function(){
+	return gulp.src('app/fonts/**/*')
+	.pipe(gulp.dest('dist/fonts/'))
+});
+/*browserSync*/
+gulp.task('browserSync', function(){
+	browserSync({
+		server:{baseDir:'app/'}
+	})
+
+});
+
+/*Watcher*/
+gulp.task('watch', ['browserSync'], function(){
+	gulp.watch('app/sass/**/*.+(sass|scss)', ['compass']);		
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/css/**/*.css', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
